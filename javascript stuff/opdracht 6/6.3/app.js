0e9b346b6541245abcf01091159e720d3e30d25a
0SamoSenpai0 <9020661@student.zadkine.nl>
2022-10-21 18:21:37 +0200

    6.4

--- javascript stuff/opdracht 6/6.3/app.js
+++ javascript stuff/opdracht 6/6.3/app.js
@@ -3,3 +3,25 @@ function changecolor() {
     console.log("het werkt ook in de console");
     document.getElementById("tekst1").style.color = "red";
 }
+function changecolor2(){
+    document.getElementById("tekst2").style.color = "blue";
+}
+function calculator(num1, num2) {
+    let answer = num1 + num2;
+    console.log(answer);
+}
+calculator(3, 12);
+
+function multiplyByFive(num1) {
+    let answer = num1 * 5;
+    console.log(answer);
+    alert(answer)
+}
+multiplyByFive(2);
+
+function calculateminutes(num1) {
+    let answer = num1 / 60;
+    console.log(answer);
+    alert(answer);
+}
+calculateminutes(6900);
